a19680fd5f471eda83f626d7d964799798a61b56
KOTOKOU Komla David <kotokou74@gmail.com>
2023-09-29 10:24:41 +0000

    create api with rest_framework and setup filtering and search

---
+++ todo/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from todo.models import Todo, TodoList
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = "__all__"
+
+
+
+class TodoListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TodoList
+        fields = "__all__"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+from todo.models import Todo, TodoList
+from todo.serializers import TodoListSerializer, TodoSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['due_date', 'favourite', "completed", "list"]
+    search_fields = ["title"]
+
+
+class TodoListViewSet(viewsets.ModelViewSet):
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
